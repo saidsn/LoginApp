ba696c7cf2a9e380e4c6a37798c71c636d5f1006
saidsn <saidsn@code.edu.az>
2024-03-13 23:36:41 +0400

    Setup TailwindCss and Create react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,49 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Username from "./components/Username";
+import Password from "./components/Password";
+import Reset from "./components/Reset";
+import Register from "./components/Register";
+import Recovery from "./components/Recovery";
+import Profile from "./components/Profile";
+import PageNotFound from "./components/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/username",
+    element: <Username />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
+
 function App() {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <main className="text-3xl font-bold underline">
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
